f8bf35c986c8199fdf1756dae2c6741d6cb9b2cb
craig beyers <cbeyers@c5r5s9.wethinkcode.co.za>
2020-06-25 17:13:40 +0200

    day03

---
+++ ex05/ft_putstr.c/ft_putstr.c
@@ -0,0 +1,22 @@
+
+void ft_putstr(char *str);
+{
+    int i;
+	
+	i = 0;
+	while(str[i] != '\0')
+    {
+	    write(1, &str[i], 1);
+		i++;
+	}		
+}
+/*
+int main(void)
+{
+    char *s1;
+
+	s1 = "hello";
+	ft_putstr(s1);
+	return 0;
+}
+*/
